057d1dbf53f5457754c2df151df994655369926b
Michael Liu <Michael20061120@163.com>
2026-01-17 17:48:04 -0500

    background

--- app/(tabs)/scotty.tsx
+++ app/(tabs)/scotty.tsx
@@ -24,72 +24,72 @@ export default function Index() {
     }
   }, []);
   return (
-    <SafeAreaView style={styles.container}>
-      <Image source={require("@/assets/images/scottyScreen.png")} />
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Image source={require("@/assets/images/scottyBackground.png")} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1 },
-  image: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain",
-  },
-  overlayContent: {
-    flex: 1,
-    paddingHorizontal: 24,
-    paddingTop: 48,
-    alignItems: "center",
-    justifyContent: "flex-start",
-  },
-  moneyIcon: {
-    position: "absolute",
-    top: 12,
-    right: 16,
-    zIndex: 10,
-  },
-  logo: {
-    position: "absolute",
-    top: 52,
-    left: 16,
-    zIndex: 10,
-  },
-  pathway1: {
-    position: "absolute",
-    top: 510,
-    zIndex: 10,
-  },
-  pathway2: {
-    position: "absolute",
-    top: 372,
-    zIndex: 10,
-  },
-  contentInner: {
-    flexDirection: "row",
-    width: "100%",
-    justifyContent: "space-between",
-    alignItems: "flex-end",
-    flex: 1,
-    paddingBottom: 16,
-    zIndex: 10,
-  },
-  scottyContainer: { alignItems: "center", bottom: 161 },
-  scottyGradient: {
-    position: "absolute",
-    bottom: 0,
-    left: -10,
-    right: -10,
-    height: 96,
-    zIndex: -1,
-  },
-  scottyImage: { width: 128, height: 128 },
-  dogHouseContainer: {
-    flex: 1,
-    alignItems: "flex-end",
-    marginRight: -48,
-    bottom: 250,
-  },
-  dogHouseImage: { width: 128, height: 128 },
+  container: { flex: 1 }
+  // image: {
+  //   width: "100%",
+  //   height: "100%",
+  //   resizeMode: "contain",
+  // },
+  // overlayContent: {
+  //   flex: 1,
+  //   paddingHorizontal: 24,
+  //   paddingTop: 48,
+  //   alignItems: "center",
+  //   justifyContent: "flex-start",
+  // },
+  // moneyIcon: {
+  //   position: "absolute",
+  //   top: 12,
+  //   right: 16,
+  //   zIndex: 10,
+  // },
+  // logo: {
+  //   position: "absolute",
+  //   top: 52,
+  //   left: 16,
+  //   zIndex: 10,
+  // },
+  // pathway1: {
+  //   position: "absolute",
+  //   top: 510,
+  //   zIndex: 10,
+  // },
+  // pathway2: {
+  //   position: "absolute",
+  //   top: 372,
+  //   zIndex: 10,
+  // },
+  // contentInner: {
+  //   flexDirection: "row",
+  //   width: "100%",
+  //   justifyContent: "space-between",
+  //   alignItems: "flex-end",
+  //   flex: 1,
+  //   paddingBottom: 16,
+  //   zIndex: 10,
+  // },
+  // scottyContainer: { alignItems: "center", bottom: 161 },
+  // scottyGradient: {
+  //   position: "absolute",
+  //   bottom: 0,
+  //   left: -10,
+  //   right: -10,
+  //   height: 96,
+  //   zIndex: -1,
+  // },
+  // scottyImage: { width: 128, height: 128 },
+  // dogHouseContainer: {
+  //   flex: 1,
+  //   alignItems: "flex-end",
+  //   marginRight: -48,
+  //   bottom: 250,
+  // },
+  // dogHouseImage: { width: 128, height: 128 },
 });
